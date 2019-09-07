f7b2be8a0425b9dd1942b4fdba3703c0c24b05a4
zhu463720742 <zbhjason463@163.com>
2019-09-07 15:12:54 +0800

    Signed-off-by: zhu463720742 <zbhjason463@163.com>

---
+++ ideszhtc/src/main/java/com/idszhtc/ideszhtc/IdeszhtcApplication.java
@@ -0,0 +1,13 @@
+package com.idszhtc.ideszhtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeszhtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeszhtcApplication.class, args);
+    }
+
+}
---
+++ ideszhtc/src/test/java/com/idszhtc/ideszhtc/IdeszhtcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.idszhtc.ideszhtc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdeszhtcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
